6f3e244fa88e68a052de7e8d5baf0789cdb26ace
zurysantos <zusanttos@gmail.com>
2026-05-22 02:19:44 +0000

    Restaurando repositório tecnológico

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <iframe
+      src="/repositorio_tecnologias.html"
+      title="Repositório Tecnológico"
+      style={{
+        width: '100vw',
+        height: '100vh',
+        border: 'none'
+      }}
+    />
+  )
+}
+
+export default App
